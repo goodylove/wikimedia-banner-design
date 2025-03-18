7b035be594edb5b0a3bc77e3eaef4b48b5b4a5f9
goodylove <goodyc474@gmail.com>
2025-03-18 19:25:21 +0100

    feat: Add banner images to public folder

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+  return <div className="banner-container">
+    <h1 className="mood">You are radiating happiness! 😊"</h1>
+  </div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./styles/global.css";
+import MoodBanner from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoodBanner />
   </React.StrictMode>
 );
 
